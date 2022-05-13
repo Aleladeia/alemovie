97b79d9d9fb1de94b6447e78894f8decd4ab12e7
Alexandre Jose <aleladeia99@gmail.com>
2022-05-13 01:23:54 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,37 @@
-import Pagination from "components/Navbar/Pagination";
+import Pagination from "components/Pagination";
+import Moviecard from "components/MovieCard";
 
 function Listing() {
 
-    return(
-        <Pagination />
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                </div>
+            </div>
+
+
+
+            <Moviecard />
+
+        </>
     );
 }
 
