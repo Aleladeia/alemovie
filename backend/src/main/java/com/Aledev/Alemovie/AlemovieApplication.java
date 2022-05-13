347906d04375ee21adad0f043907d17c5d8b6cb8
Alexandre Jose <aleladeia99@gmail.com>
2022-05-12 21:24:30 -0300

    Project created

---
+++ backend/src/main/java/com/Aledev/Alemovie/AlemovieApplication.java
@@ -0,0 +1,13 @@
+package com.Aledev.Alemovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlemovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlemovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Aledev/Alemovie/AlemovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aledev.Alemovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlemovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
